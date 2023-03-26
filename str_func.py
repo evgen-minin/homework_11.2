b6ea635eed58dfba3ca50265e5236b4c2fb5483f
Евгений Минин <evgenminin1988@mail.ru>
2023-03-26 18:25:58 +0300

    comment

--- str_func.py
+++ str_func.py
@@ -1,4 +1,18 @@
 def changes_letter_case(text):
+    """
+    comment
+    :param text:
+    :return:
+    """
     return text.upper()
 
 print(changes_letter_case("hello skypro"))
+
+
+def changes_letter_case_2(text):
+    """
+    comment
+    :param text:
+    :return:
+    """
+    return ' '.join(map(lambda x: x.capitalize(), text.split(' ')))
